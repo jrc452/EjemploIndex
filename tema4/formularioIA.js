0dd71954feba47d8981f8e2c82662c9e7ce7c980
jrc452 <jairoreyescalvario@gmail.com>
2025-03-28 22:26:51 +0100

    Update formularioIA

--- tema4/formularioIA.js
+++ tema4/formularioIA.js
@@ -1,7 +1,7 @@
-function collectData() {
-    var nombre = prompt('Por favor, introduce tu nombre:');
-    var edad = prompt('Por favor, introduce tu edad:');
-    var correo = prompt('Por favor, introduce tu correo electrónico:');
-    document.getElementById('result').innerHTML =
-        '<h2>RESALTDOS:</h2><p><font style="font-weight:bold">Nombre:</font> ' + nombre + '</p><p><font style="font-weight:bold">Edad:</font> ' + edad + '</p><p><font style="font-weight:bold">Correo:</font> ' + correo + '</p>';
-}
+function submitForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var gender = document.querySelector('input[name="gender"]:checked') ? document.querySelector('input[name="gender"]:checked').value : 'Not specified';
+    var message = 'Nombre: ' + name + '\nCorreo: ' + email + '\nGenero: ' + gender;
+    prompt('DATOS DEL FORMULARIO:', message);
+};
